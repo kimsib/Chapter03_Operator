f6c611179a798ae111dfd6efe17f5418c8d30083
bitcamp <bitcamp@DESKTOP-J4V03M8>
2021-08-12 19:20:47 +0900

    commit

--- src/ch03_1_operator_expression/CompareOperatorExample2.java
+++ src/ch03_1_operator_expression/CompareOperatorExample2.java
@@ -4,13 +4,12 @@ public class CompareOperatorExample2 {
 	public static void main(String[] args) {
 		int v2 = 1;
 		double v3 = 1.0;
-		sys
-		
-		
-		
-		
-		
-		
-		
-}
+		System.out.println(v2 == v3);
+
+		double v4 = 0.1;
+		float v5 = 0.1f;
+		System.out.println(v4 == v5);
+		System.out.println((float) v4 == v5);
+
+	}
 }
